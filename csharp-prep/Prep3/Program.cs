155923a9a42ba473d1d29d8cf9d00bdc387d4c1d
NolanMonnier <mon21025@byui.edu>
2023-01-10 17:55:58 -0700

    Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        int tries = 0;
+        string play = "Y";
+        Console.WriteLine("");
+
+        while (play == "Y" || play == "y" || play == "Yes" || play == "yes")
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 100);
+            for (int guess = 0; guess != number;)
+            {
+
+                Console.Write("What is your guess? ");
+                string stGuess = Console.ReadLine();
+                guess = int.Parse(stGuess);
+
+                if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+
+                else if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                else if (guess == number)
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+
+                tries += 1;
+            }
+            Console.WriteLine($"You found the magic number in {tries} tries.");
+
+            Console.Write("Do you want to play again? Y/N: ");
+            play = Console.ReadLine();
+        }
+
     }
 }
