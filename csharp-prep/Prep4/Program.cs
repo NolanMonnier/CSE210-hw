982cf75147d7ec9872cade40b5127ca7294bd897
NolanMonnier <mon21025@byui.edu>
2023-01-10 18:34:57 -0700

    Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numberList = new List<int>();
+        int numbers = -1;
+        int sum = 0;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (numbers != 0)
+        {
+            Console.Write("Enter number: ");
+            numbers = int.Parse(Console.ReadLine());
+            if (numbers != 0)
+            {
+                numberList.Add(numbers);
+                sum += numbers;
+            }
+        }
+        Console.WriteLine($"The sum is {sum}");
+
+        float average = ((float)sum) / numberList.Count;
+        Console.WriteLine($"The average is {average}");
+
+        int max = numberList.Max();
+        Console.WriteLine($"The largest number is {max}");
     }
 }
