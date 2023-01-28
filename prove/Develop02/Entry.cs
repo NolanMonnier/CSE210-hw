bc02addd153a4e00c84b0650c6aae8ae29aa5c87
NolanMonnier <mon21025@byui.edu>
2023-01-28 12:11:54 -0700

    added follow up question

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -8,8 +8,8 @@ public class Entry
     public string _prompt1 = "What was the highlight of my day?";
     public string _prompt2 = "How did I feel the love of my loved ones today?";
     public string _prompt3 = "What did I do to get closer to the Lord today?";
-    public string _prompt4 = "If I could do one thing again today, what would I do?";
-    public string _prompt5 = "What wasI successful at today?";
+    public string _prompt4 = "What did you do to bring joy into your life today?";
+    public string _prompt5 = "What was I successful at today?";
 
 
 
@@ -30,35 +30,45 @@ public class Entry
             Console.WriteLine($"{_prompt1}");
             Console.Write("Entry: ");
             string _answer = Console.ReadLine();
-            _finalAnswer = "Date " + entryDate + " Prompt: " + _prompt1 + " Entry: " + _answer + "\n" + "\n";
+            Console.Write("How did that make you feel? ");
+            string _followUp = Console.ReadLine();
+            _finalAnswer = "Date " + entryDate + " Prompt: " + _prompt1 + "\n" + "Entry: " + _answer + "\n" + "Feelings" + _followUp + "\n \n";
         }
         if (number == 2)
         {
             Console.WriteLine($"{_prompt2}");
             Console.Write("Entry: ");
             string _answer = Console.ReadLine();
-            _finalAnswer = "Date " + entryDate + " Prompt: " + _prompt2 + " Entry: " + _answer + "\n" + "\n";
+            Console.Write("How did that make you feel? ");
+            string _followUp = Console.ReadLine();
+            _finalAnswer = "Date " + entryDate + " Prompt: " + _prompt2 + "\n" + "Entry: " + _answer + "\n" + "Feelings" + _followUp + "\n \n";
         }
         if (number == 3)
         {
             Console.WriteLine($"{_prompt3}");
             Console.Write("Entry: ");
             string _answer = Console.ReadLine();
-            _finalAnswer = "Date " + entryDate + " Prompt: " + _prompt3 + "Entry: " + _answer + "\n" + "\n";
+            Console.Write("How did that make you feel? ");
+            string _followUp = Console.ReadLine();
+            _finalAnswer = "Date " + entryDate + " Prompt: " + _prompt3 + "\n" + "Entry: " + _answer + "\n" + "Feelings" + _followUp + "\n \n";
         }
         if (number == 4)
         {
             Console.WriteLine($"{_prompt4}");
             Console.Write("Entry: ");
             string _answer = Console.ReadLine();
-            _finalAnswer = "Date " + entryDate + " Prompt: " + _prompt4 + " Entry: " + _answer + "\n" + "\n";
+            Console.Write("How did that make you feel? ");
+            string _followUp = Console.ReadLine();
+            _finalAnswer = "Date " + entryDate + " Prompt: " + _prompt4 + "\n" + "Entry: " + _answer + "\n" + "Feelings" + _followUp + "\n \n";
         }
         if (number == 5)
         {
             Console.WriteLine($"{_prompt5}");
             Console.Write("Entry: ");
             string _answer = Console.ReadLine();
-            _finalAnswer = "Date " + entryDate + " Prompt: " + _prompt1 + "  Entry: " + _answer + "\n" + "\n";
+            Console.Write("How did that make you feel? ");
+            string _followUp = Console.ReadLine();
+            _finalAnswer = "Date " + entryDate + " Prompt: " + _prompt5 + "\n" + "Entry: " + _answer + "\n" + "Feelings" + _followUp + "\n \n";
         }
 
         return _finalAnswer;
