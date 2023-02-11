ae963cab4ae7ffb5ddd85bea1d0548081ae6cf48
NolanMonnier <mon21025@byui.edu>
2023-02-10 17:08:28 -0700

    End

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections.Generic;
-
+//To exceed the requirement, I asked the user at the beginning how many words they would like to hide. 
 class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine();
         Console.WriteLine("Welcome to the scripture memorizer");
+        Console.Write("Please enter the largest amount of words you would like to hide: ");
+        string _countStr = Console.ReadLine();
+        int _count = int.Parse(_countStr);
         List<string> verses = new List<string>
         {
             "Trust in the Lord with all thine heart and lean not unto thine own understanding",
@@ -17,20 +21,28 @@ class Program
         Console.WriteLine("Here is your scripture:");
         bool _isAllHiden = scripture.IsAllHidden();
 
-        while (_answer != "Quit" && _isAllHiden != true)
+        while (_isAllHiden != true)
         {
             scripture.Display();
             Console.WriteLine(" ");
-            Console.Write("Please press Enter to hide words, or write Quit to quit");
-            scripture.HideWords(3);
+            Console.Write("Please press Enter to hide words, or write Quit to quit: ");
+            scripture.HideWords(_count);
             _answer = Console.ReadLine();
             Console.WriteLine(" ");
+            if (_answer == "Quit")
+            {
+                break;
+            }
             _isAllHiden = scripture.IsAllHidden();
         }
-        scripture.Display();
-        Console.WriteLine(" ");
-        Console.Write("Please press enter to hide words, or write Quit to quit");
-        Console.ReadLine();
+
+        if (_isAllHiden == true)
+        {
+            scripture.Display();
+            Console.WriteLine(" ");
+            Console.Write("Please press enter to hide words, or write Quit to quit: ");
+            Console.ReadLine();
+        }
 
 
         Console.WriteLine("Quitting...");
