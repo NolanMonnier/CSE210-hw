24b2233ca16fba83a7f5753df4583514985591bc
NolanMonnier <mon21025@byui.edu>
2023-02-14 20:29:35 -0700

    Assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Samuel Bennett", "Multiplication");
+        System.Console.WriteLine(assignment1.GetSummary());
+        System.Console.WriteLine();
+
+        MathAssignment mathAssignment1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-9");
+        System.Console.WriteLine(mathAssignment1.GetSummary());
+        System.Console.WriteLine(mathAssignment1.GetHomeworkList());
+        System.Console.WriteLine();
+
+        WritingAssignment writingAssignment1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
+        System.Console.WriteLine(writingAssignment1.GetSummary());
+        System.Console.WriteLine(writingAssignment1.GetHomeworkList());
+
     }
 }
