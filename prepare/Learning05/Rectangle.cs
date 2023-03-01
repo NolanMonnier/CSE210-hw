3fb5928433029fdaad3bf82c5330523fd88458d5
NolanMonnier <mon21025@byui.edu>
2023-02-28 18:09:47 -0700

    Leanring 05

---
+++ prepare/Learning05/Rectangle.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Rectangle : Shape
+{
+    private double _width = 0;
+    private double _length = 0;
+
+    public Rectangle(string color, double width, double length) : base(color)
+    {
+        _width = width;
+        _length = length;
+    }
+    public override double GetArea()
+    {
+        return _width * _length;
+    }
+}
